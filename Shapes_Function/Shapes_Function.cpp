b8039081e8b485c385115e2d0a7bc529e17ffb1d
baris <baris@BARIS_SOMEROGLU>
2024-04-20 22:12:13 +0300

    Add project files.

---
+++ Shapes_Function/Shapes_Function.cpp
@@ -0,0 +1,49 @@
+﻿/*
+This code shows a shape whic made with functions.
+
+Developer = Barış Someroğlu 
+Date = 20.04.2024 - 10:00 pm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Prototype of function
+void DrawSquare();
+void DrawTriangle();
+
+int main()
+{
+	// Call the function
+	DrawSquare();
+	DrawTriangle();
+
+	return 0;
+}
+
+// Function definition
+void DrawSquare()
+{
+	cout << "----------" << endl;
+	cout << "|         |" << endl;
+	cout << "| 0     0 |" << endl;
+	cout << "|    *    |" << endl;
+	cout << "| ------  |" << endl;
+	cout << "----------" << endl;
+}
+
+// Function definition
+void DrawTriangle()
+{
+	cout << "    / \\  " << endl;
+	cout << "   /   \\ " << endl;
+	cout << "  /  no \\" << endl;
+	cout << " /  hand \\ " << endl;
+	cout << "/         \\ " << endl;
+	cout << "-----------" << endl;
+	cout << " |       | " << endl;
+	cout << " |       | " << endl;
+	cout << " |       | " << endl;
+	cout << " --      -- " << endl;
+}
